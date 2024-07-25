d6c4fee4a375f8ab7fee5ca515b8f5bc5a91d139
Aeron <raiaseem25@gmail.com>
2024-07-25 09:04:09 +0545

    feat:Lime

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./index.css";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -7,8 +8,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={Login} />
-        <Route path="/signup" element={Signup} />
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,38 @@
+import { Button } from "@/components/ui/button.jsx";
+import { Card, CardHeader } from "@/components/ui/card.jsx";
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <section className="w-full">
+      <div className="w-full h-screen bg-login-img bg-cover flex justify-center items-center">
+        <Card className="w-1/4 rounded-2xl shadow-lg">
+          <CardHeader className="mt-2 text-5xl font-Reddit font-extrabold">
+            WELCOME
+          </CardHeader>
+          <div>
+            <input
+              type="email"
+              id="email"
+              className="w-3/4 ml-7 my-3 border-b-2 border-black text-xl"
+              placeholder="Email"
+            />
+            <input
+              type="password"
+              id="password"
+              className="w-3/4 ml-7 my-7 border-b-2 border-black text-xl"
+              placeholder="Password"
+            />
+          </div>
+          <Button className="w-40 bg-black rounded-full m-auto text-lg flex justify-center items-center">
+            LOGIN
+          </Button>
+          <h2 className="my-6 text-base text-stone-400 ml-24">
+            Dont have a account? SignUp
+          </h2>
+        </Card>
+      </div>
+    </section>
+  );
 };
 export default Login;
